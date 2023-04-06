bd768d8b97c995094ba169877f44f5316f00eb4e
FilPag <filip.pagliaro.4567@student.uu.se>
2023-04-06 17:17:07 +0200

    fixed logging error

--- mappo/runner/shared/mpe_runner.py
+++ mappo/runner/shared/mpe_runner.py
@@ -100,14 +100,15 @@ class MPERunner(Runner):
                     agent_k = 'agent%i/individual_rewards' % agent_id
                     env_infos[agent_k] = idv_rews
 
+                self.log_env(env_infos, total_num_steps)
             train_infos["average_episode_rewards"] = np.mean(
                 self.buffer.rewards)
+
             if ray.tune.is_session_enabled():
                 session.report({"average_episode_rewards": train_infos["average_episode_rewards"]})
             print("average episode rewards is {}".format(
                 train_infos["average_episode_rewards"]))
             self.log_train(train_infos, total_num_steps)
-            self.log_env(env_infos, total_num_steps)
 
           # eval
         self.writter.add_scalar('communication_savings', 1 - tot_comms / (self.episode_length * self.num_agents * self.n_rollout_threads), episode)
